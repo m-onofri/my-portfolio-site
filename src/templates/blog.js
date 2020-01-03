b9ad38bedc19f35e662927d9469e4538cb77a4c9
m-onofri <manuele.onofri@gmail.com>
2020-01-03 22:35:11 +0100

    update footer

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,8 @@
-import React from 'react';
-// import { graphql } from 'gatsby';
-import Layout from '../components/layout';
-import PageHeaderBlog from '../components/blog/pageHeaderBlog';
-import PostContent from '../components/blog/postContent';
+// import React from 'react';
+// // import { graphql } from 'gatsby';
+// import Layout from '../components/layout';
+// import PageHeaderBlog from '../components/blog/pageHeaderBlog';
+// import PostContent from '../components/blog/postContent';
 
 // export const query = graphql`
 //     query($slug: String!) {

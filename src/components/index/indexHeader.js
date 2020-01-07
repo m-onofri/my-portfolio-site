57b1f35e0c0293777fbd6ee2e55455f27a1d0c5a
m-onofri <manuele.onofri@gmail.com>
2020-01-07 10:07:00 +0100

    update index header

--- src/components/index/indexHeader.js
+++ src/components/index/indexHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'gatsby';
 import homeBg from '../../dist/img/home-bg.jpg'
 
 const IndexHeader = () =>
@@ -9,8 +10,9 @@ const IndexHeader = () =>
                 <div className="col-lg-8 col-md-10 mx-auto">
                     <div className="site-heading">
                         <h1>Hi I'm Manuele</h1>
-                        <span className="subheading">I like programming in javaScript and PHP</span>
-                        <span className="subheading">Give a look at my portfolio</span>
+                        <span className="subheading">I'm a junior developer and programmer based in Rimini, Italy.</span>
+                        <span className="subheading">I've built web applications using PHP, Node.js and React and have good foundation in web design and development.</span>
+                        <Link id="all_pjs" className="button" to="/about">More About Me</Link>
                     </div>
                 </div>
             </div>
